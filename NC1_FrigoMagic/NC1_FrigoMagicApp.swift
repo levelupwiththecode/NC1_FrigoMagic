e7c14830046089e77854106abad07d4200048e5e
levelupwiththecode <massidedosso@gmail.com>
2022-11-17 12:33:30 +0100

    Initial Commit

---
+++ NC1_FrigoMagic/NC1_FrigoMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_FrigoMagicApp.swift
+//  NC1_FrigoMagic
+//
+//  Created by Massidé Dosso on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NC1_FrigoMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
